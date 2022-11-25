2c04972481b55db622143bc6e4a93c515898a722
Monophano <89103355+Monophano@users.noreply.github.com>
2022-11-25 08:52:57 +0100

    setup du bg

--- src/main.py
+++ src/main.py
@@ -1,2 +1,25 @@
 #import du modules
-import pygame
+from pygame import *
+
+#création d'une fenetre de 360 par 480
+game_fen = display.set_mode((360,480))
+
+#définition du titre de la fenetre
+display.set_caption("SMSIdle")
+
+#init la fen
+init()
+
+#setup du bg
+bg = image.load('')
+bg = bg.convert()
+
+#condition de continuation du jeu
+jouer = True
+
+while jouer == True:
+    #si fenetre quitter alors fin du jeux
+    for events in event.get():
+         if events.type == QUIT:
+             jouer=False
+             quit()
