ec438cc781b8dd207d57f17a71d3b0de883d048f
Monophano <89103355+Monophano@users.noreply.github.com>
2022-12-06 13:04:24 +0100

    tkt encore

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
-#import du modules
-import pygame
+#import de bibliothèques
+import pygame, sys
+from random import *
+
+#import des modules de jeux
+
+#initiation de pygame
+pygame.init()
+
+#paramètres de l'écran
+size = width, height = 320, 240
+
+#afficher l'écran
+screen = pygame.display.set_mode(size)
+
+#Game running
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
