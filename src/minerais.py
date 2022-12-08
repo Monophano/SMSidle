b23fbf9a8bd8c3e5c1302a83b9001f7058ff9bf6
Monophano <89103355+Monophano@users.noreply.github.com>
2022-12-08 13:18:00 +0100

    ajout des minerais

--- src/minerais.py
+++ src/minerais.py
@@ -1,3 +1,15 @@
 #import de bibliothèques
 from random import *
 import pygame
+
+class Minerais():
+    def __init__(self):
+        self.roche = 1
+        self.cuivre = 3
+        self.fer = 5
+        self.gold = 10
+        self.diamant = 15
+        self.emeraude = 20
+        self.tungsten = 30
+
+    def  rn_
